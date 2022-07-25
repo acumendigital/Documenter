0c226abb89e57d61380fc272d67d76547953ee12
Oluwakemi Oyindamola Akindele <brandykemi@gmail.com>
2022-07-25 10:51:41 +0100

    Added table components

---
+++ components/Basics.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="container">
+      <div
+        class="content"
+        @click="
+          clicked = true
+          $router.push('#')
+        "
+      >
+        <img src="~/assets/images/slider.svg" />
+        <p>
+          {{ title }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+  props: {
+    title: String,
+  },
+}
+</script>
+<style scoped>
+.container {
+  margin-top: 24px;
+}
+.image img {
+  margin-bottom: 0 !important;
+}
+.content {
+  display: flex;
+  align-items: center;
+  /* width: 359px; */
+  background: #fbfbfb;
+  border: 1px solid #efefef;
+  border-radius: 8px;
+  padding: 0 16px;
+  cursor: pointer;
+}
+.content p {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 18px;
+  color: #51545c;
+  padding-left: 26.75px;
+}
+.content:hover {
+  background: #f5f7fc;
+}
+.content p:hover {
+  color: #9999bc;
+}
+</style>
