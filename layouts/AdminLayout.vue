46189fff2e5aa958ec978a37298718f3f38acbd8
SualaDev <81395134+SualaDev@users.noreply.github.com>
2022-07-27 07:07:23 +0100

    admin layout done

---
+++ layouts/AdminLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+      <AdminSidebar />
+    <div class="main-layout">
+      <div class="header">
+        <AdminHeader />
+      </div>
+      <div class="main-content">
+        <Nuxt />
+      </div>               
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'AdminLayout'
+  }
+</script>
+
+<style scoped>
+.main{
+  display: flex;
+}
+.main-layout{
+  /* width: calc(100% - 270px);
+  margin-left: 270px; */
+}
+.main-content{
+  background-color: #FFF;
+}
+</style>
