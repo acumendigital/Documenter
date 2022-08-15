dc9cd2c89d1ef3bef579cebcaec9bcb43ae4d140
SualaDev <81395134+SualaDev@users.noreply.github.com>
2022-08-15 13:11:16 +0100

    worked on the picture card component

--- dist/_nuxt/60a2875.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{405:function(n,e,o){"use strict";o.r(e);var t={components:{home:o(401).default},name:"IndexPage"},l=o(9),component=Object(l.a)(t,(function(){return(0,this._self._c)("home")}),[],!1,null,null,null);e.default=component.exports}}]);
