d46d1b90451d47880c0055a467188f971321a96a
Harith Onigemo <onigemotosin@gmail.com>
2022-08-17 10:56:53 +0100

    :sparkles: Introduced the Code block component

---
+++ components/reusables/TheCodeBlock.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="code-block-container">
+        <textarea name="code-block" id="code-block" ref="codeBlock" cols="30" rows="5"></textarea>
+    </div>
+</template>
+
+<script>
+    import * as Codemirror from 'codemirror'
+    import 'codemirror/lib/codemirror.css'
+    import 'codemirror/theme/dracula.css'
+    import 'codemirror/mode/javascript/javascript.js'
+export default {
+    data(){
+        return{
+            code: '',
+            codeMirrorInstance: ''
+        }
+    },
+    mounted(){
+        this.codeMirrorInstance = Codemirror.fromTextArea(this.$refs.codeBlock, {
+            // lineNumbers: true,
+            theme: 'dracula',
+            mode: 'javascript',
+            autocompletion: true,
+        })
+    }
+}
+</script>
+
+<style>
+    .CodeMirror{
+        height: auto;
+        border-radius: 8px;
+        padding: 16px;
+        border: 2px solid red;
+    }
+</style>
