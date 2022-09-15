e9d30a654facc78e89f0c04733ce6f5e34a81d08
Harith Onigemo <onigemotosin@gmail.com>
2022-09-15 18:41:02 +0400

    :zap: used regex to match text for displaying codeblock - Admin

--- components/reusables/TheReusableBlock.vue
+++ components/reusables/TheReusableBlock.vue
@@ -5,20 +5,27 @@
          @hide-options="hideOptions" 
          v-show="blockDisplayed == 'Text'" 
          @update-block-index="updateIndex"
+         :index='index'
         />
-        <the-page-title-block v-show="blockDisplayed == 'page-title'" />
-        <!-- <the-code-block-with-tabs v-show="blockDisplayed == 'Code Block'" /> -->
-        <the-notes-container v-show="blockDisplayed == 'Note'"  />
-        <the-warning-card v-show="blockDisplayed == 'Warning'" />
-        <the-info-card v-show="blockDisplayed == 'Info'" />
-        <the-divider-component v-show="blockDisplayed == 'Divider'" />
-        <the-code-block v-show="blockDisplayed == 'Code Block'" />
+        <the-page-title-block v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'page-title'" :index='index' />
+        <the-notes-container v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Note'" :index='index' />
+        <the-warning-card v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Warning'" :index='index' />
+        <the-info-card v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Info'" :index='index' />
+        <the-divider-component v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Divider'" :index='index' />
+        <the-code-block v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Code Block'" :index='index' />
+        <admin-codeblock-with-response v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Code & Res'" :index='index' />
+        <the-card v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Card'" :index='index' />
+        <admin-code-block-with-tabs v-show="blockDisplayed.replace(/\s([^a-z])$/, '') == 'Code Table'" :index='index' />
     </div>
 </template>
 
 <script>
 import Notes from '../Notes.vue'
+import AdminCodeblockWithResponse from './AdminCodeblockWithResponse.vue';
+import AdminCodeBlockWithTabs from './AdminCodeBlockWithTabs.vue';
+import TheCard from './TheCard.vue';
 import TheCodeBlock from './TheCodeBlock.vue';
+import TheCodeBlockWithResponse from './TheCodeBlockWithResponse.vue';
 import TheCodeBlockWithTabs from './TheCodeBlockWithTabs.vue';
 import TheDividerComponent from './TheDividerComponent.vue';
 import TheInfoCard from './TheInfoCard.vue';
@@ -27,13 +34,7 @@ import TheNotesContainer from './TheNotesContainer.vue';
 import ThePageTitleBlock from './ThePageTitleBlock.vue'
 import TheWarningCard from './TheWarningCard.vue';
 export default {
-    components: { ThePageTitleBlock, Notes, TheNormalText, TheCodeBlockWithTabs, TheNotesContainer, TheWarningCard, TheInfoCard, TheDividerComponent, TheCodeBlock },
-    data(){
-        return{
-            showModal: false,
-            blockDisplayed: 'Text'
-        }  
-    },
+    components: { ThePageTitleBlock, Notes, TheNormalText, TheCodeBlockWithTabs, TheNotesContainer, TheWarningCard, TheInfoCard, TheDividerComponent, TheCodeBlock, TheCodeBlockWithResponse, AdminCodeblockWithResponse, TheCard, AdminCodeBlockWithTabs },
     props:{
         blockType:{
             type: String,
@@ -48,8 +49,21 @@ export default {
         blockType:{
             handler(newValue, oldValue){
                 this.blockToDisplay(newValue);
+                console.log(newValue);
             }
-        }
+        },
+    },
+    data(){
+        return{
+            showModal: false,
+            blockDisplayed: `Text ${this.index}`
+        }  
+    },
+    mounted(){
+        // Set the blockdisplayed to the current blocktype
+        // Used a regex to match all group of characters without a space and a digit
+        this.blockDisplayed = this.blockType.replace(/\s([^a-z])$/, '');
+        // window.localStorage.removeItem('vuex');
     },
     methods:{
         // Event handler for the enter button
@@ -59,7 +73,8 @@ export default {
         },
         // Event handler when an option is picked from the options modal
         blockToDisplay(block){
-            this.blockDisplayed = block;
+            // Used a regex to match all group of characters without a space and a digit
+            this.blockDisplayed = block.replace( /\s([^a-z])$/, '');
             this.hideOptions();
             console.log("Block changed to " + block);
         },
@@ -74,7 +89,7 @@ export default {
         updateIndex(){
             this.$emit('update-block-index', this.index);
         }
-    }
+    },
 }
 </script>
 
